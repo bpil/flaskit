75c3dae6dbdb282b021796049ca1f56deb2ac516
bpilat <bastien@bpilat.net>
2017-07-12 00:15:08 +0200

    bpi

--- flaskit.py
+++ flaskit.py
@@ -4,18 +4,48 @@ import os,sys,string,time,json,urllib2
 from glob import glob
 from pymongo import MongoClient
 
+from jinja2 import Environment, FileSystemLoader
+from jinja2schema import infer, to_json_schema
+
 from flask import Flask, request, make_response, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	return 'Hello World!'
+def root():
+	response = make_response(jsonify(code='OK', comment='Available apps : /templatize'), 200)
+	response.headers['Content-Type'] = 'application/json'
+	return response
 
-
+@app.route('/templatize')
+def template_root():
+	fileList=glob("templates/*.j2")
+	tmpCode = 'OK'
+	templates = []
+	for fileName in fileList:
+		templateName=fileName.split('/')[1].rstrip('.j2')
+		templates = templates + [templateName]
+	response = make_response(jsonify(code=tmpCode, templates=templates), 200)
+	response.headers['Content-Type'] = 'application/json'
+	return response
+	
 @app.route('/templatize/<template_type>', methods=['GET', 'POST'])
 def templatize(template_type):
+	jinjaEnv = Environment(loader=FileSystemLoader('/home/bpilat/dev/flaskit/templates/'))
 	if request.method == 'GET':
-		response = make_response(jsonify(code='OK', template=template_type), 200)
+		fileList=glob("templates/*.j2")
+		code = 'NOK'
+		reason = 'Invalid template'
+		jsonSchema = ""
+		for fileName in fileList:
+			templateName = fileName.split('/')[1].rstrip('.j2')
+			if template_type == templateName:
+				code = 'OK'
+				reason = 'OK'
+				template = jinjaEnv.loader.get_source(jinjaEnv, template_type + '.j2')[0]
+				s = infer(template)
+				jsonSchema = to_json_schema(s)
+		response = make_response(jsonify(code=code, reason=reason, params=jsonSchema), 200)
 		response.headers['Content-Type'] = 'application/json'
 		return response
 	if request.method == 'POST':
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-PYTHONPATH=.:$PYTHONPATH py.test -s --tb=short --showlocals \
-                                 --cov-report term-missing --cov jinja2schema ./tests "$@"
