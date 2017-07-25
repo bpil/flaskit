be780f7454d0e71f81cbe9c0d1e03a34b9e120fa
bpilat <bastien@bpilat.net>
2017-07-25 23:21:30 +0200

    bpi

--- ConfigTemplates.py
+++ ConfigTemplates.py
@@ -11,12 +11,20 @@ from flask import request, make_response, jsonify
 
 
 class ConfigTemplate(object):
+
 	def __init__(self, templateDir):
 		self.jinjaEnv = Environment(loader=FileSystemLoader( templateDir))
 		self.templateDir = templateDir
+		self.ERROR_CODES = {
+			'Unknown': 'Uknown error',
+			'Invalid Template': '',
+			'Bad Encoding': 'Request must be application/json',
+			'No Action': 'Request has no Action parameter',
+			'No Params': 'Action request without params argument',
+			'Unknown Action': 'Type of Action is not implemented'
+		}
 
 	def parseSchema(self, schema):
-	#	print schema
 		response = {}
 		if 'properties' in schema.keys():
 			response =  self.parseSchema(schema['properties'])
@@ -31,7 +39,7 @@ class ConfigTemplate(object):
 					#if 'title' in schema[k].keys():
 					else:
 						if 'items' in elt.keys():
-							response[k]=self.parseSchema(elt['items'])
+							response[k]=[ self.parseSchema(elt['items']) ]
 						else:
 							response[k] = elt['type']
 		return response
@@ -47,13 +55,31 @@ class ConfigTemplate(object):
 		response.headers['Content-Type'] = 'application/json'
 		return response
 
+	def checkTemplate(self, template_type):
+		fileList=glob(self.templateDir + "/*.j2")
+		response = False
+		for fileName in fileList:
+			templateName = fileName.split('/')[-1].rstrip('.j2')
+			if template_type == templateName:
+				response = True
+		return response
+
+	def errorCode(self, reason='Unknown'):
+		respReason = 'Unknown'
+		comment = self.ERROR_CODES[reason]
+		if reason in self.ERROR_CODES.keys():
+			respReason = reason
+			comment = self.ERROR_CODES[reason]
+		response = make_response(jsonify(code='NOK', reason=respReason, comment=comment), 400)
+		response.headers['Content-Type'] = "application/json"
+		return response
+
+
 	def responseTemplate(self, request, template_type):
 		response = make_response(jsonify(code='NOK', reason='Unknown', comment='Unkown Error'), 500)
 		response.headers['Content-Type'] = "application/json"
 		if request.method == 'GET':
 			fileList=glob(self.templateDir + "/*.j2")
-			code = 'NOK'
-			reason = 'Invalid template'
 			jsonSchema = ""
 			for fileName in fileList:
 				templateName = fileName.split('/')[-1].rstrip('.j2')
@@ -63,19 +89,27 @@ class ConfigTemplate(object):
 					source = self.jinjaEnv.loader.get_source(self.jinjaEnv, template_type + '.j2')[0]
 					s = infer(source)
 	#				jsonSchema = s
-					print to_json_schema(s, jsonschema_encoder=StringJSONSchemaDraft4Encoder)
 					jsonSchema = self.parseSchema( to_json_schema(s, jsonschema_encoder=StringJSONSchemaDraft4Encoder) )
-					print jsonSchema
-			response = make_response(jsonify(code=code, reason=reason, params=jsonSchema), 200)
-			response.headers['Content-Type'] = 'application/json'
+					response = make_response(jsonify(code=code, reason=reason, params=jsonSchema), 200)
+					response.headers['Content-Type'] = 'application/json'
+					return response
+			response = self.errorCode('Invalid Template')
 			return response
 		if request.method == 'POST':
 			if not request.is_json:
-				response = make_response(jsonify(code='NOK', template=template_type, reason='Bad format', comment='Request must be application/json'), 400)
-				response.headers['Content-Type'] = 'application/json'
+				response = self.errorCode('Bad Encoding')
 				return response
 			data = request.get_json()
-			template = self.jinjaEnv.get_template(template_type + '.j2')
-			config = template.render(data)
-			response = make_response(jsonify(code='OK', config=config), 200)
+			if not 'action' in data.keys():
+				response = self.errorCode('No Action')
+				return response
+			if data['action'] == 'render':
+				if not 'params' in data.keys():
+					response = self.errorCode('No Params')
+					return response
+				template = self.jinjaEnv.get_template(template_type + '.j2')
+				config = template.render(data['params'])
+				response = make_response(jsonify(code='OK', config=config), 200)
+				return response
+			response = self.errorCode('Unknown Action')
 			return response
